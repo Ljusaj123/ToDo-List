66ca21f64f6a2ae1300d896383b5950e9a02ce79
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-03-21 16:13:24 +0100

    functions for saving task in task list state, function for handle change

--- src/components/PopupWindow.js
+++ src/components/PopupWindow.js
@@ -3,11 +3,27 @@ import { AiOutlineClose } from "react-icons/ai";
 import DaysPopupWindow from "./DaysPopupWindow";
 import ListPopupWindow from "./ListPopupWindow";
 
-function PopupWindow({ trigger, setTrigger }) {
+function PopupWindow({ trigger, setTrigger, taskList, setTaskList }) {
+  const initialTask = { name: "", notes: "", day: "", list: "" };
+
   const [listTrigger, setListTrigger] = useState(false);
   const [dayTrigger, setDayTrigger] = useState(false);
+  const [task, setTask] = useState(initialTask);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTask({ ...task, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setTaskList([...taskList, task]);
+    setTask(initialTask);
+    setTrigger(false);
+  };
+
   return (
     <>
+      {console.log(trigger)}
       {trigger ? (
         <div className="popup">
           <div className="popup-container">
@@ -21,36 +37,61 @@ function PopupWindow({ trigger, setTrigger }) {
               <AiOutlineClose />
             </div>
             <div className="container task-name">
-              <input type="text" placeholder="Enter new task..." />
+              <input
+                type="text"
+                placeholder="Enter new task..."
+                name="name"
+                onChange={handleChange}
+              />
             </div>
             <div className="container task-description">
-              <textarea placeholder="Enter notes..."></textarea>
+              <textarea
+                placeholder="Enter notes..."
+                name="notes"
+                onChange={handleChange}
+              ></textarea>
             </div>
             <div className="day-list container">
-              <button
-                className="select-btn"
-                onClick={() => setDayTrigger(true)}
-              >
-                Select day
-              </button>
-              <DaysPopupWindow
-                trigger={dayTrigger}
-                setTrigger={setDayTrigger}
-              />
-              <button
-                className="select-btn"
-                onClick={() => setListTrigger(true)}
-              >
-                Select list
-              </button>
-              <ListPopupWindow
-                trigger={listTrigger}
-                setTrigger={setListTrigger}
-              />
+              <div className="day-container">
+                <button
+                  className="select-btn"
+                  onClick={() => setDayTrigger(true)}
+                >
+                  Select day
+                </button>
+                {task.day ? <p>Day selected: {task.day}</p> : ""}
+                <DaysPopupWindow
+                  trigger={dayTrigger}
+                  setTrigger={setDayTrigger}
+                  task={task}
+                  setTask={setTask}
+                />
+              </div>
+              <div className="list-container">
+                <button
+                  className="select-btn"
+                  onClick={() => setListTrigger(true)}
+                >
+                  Select list
+                </button>
+                {task.list ? <p>List selected: {task.list}</p> : ""}
+                <ListPopupWindow
+                  trigger={listTrigger}
+                  setTrigger={setListTrigger}
+                  task={task}
+                  setTask={setTask}
+                />
+              </div>
             </div>
 
             <div className="new-task container">
-              <button className="create-btn">Create task</button>
+              <button
+                type="submit"
+                className={task.name && task.day ? "create-btn" : "no-task-btn"}
+                onClick={handleSubmit}
+              >
+                Create task
+              </button>
             </div>
           </div>
         </div>
