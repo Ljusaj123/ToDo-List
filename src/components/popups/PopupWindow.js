e9b480f4ddf4a02bcc2e8691540573f6757447d3
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-05-10 02:24:28 +0200

    fixed error message after exiting the popup

--- src/components/popups/PopupWindow.js
+++ src/components/popups/PopupWindow.js
@@ -30,10 +30,16 @@ function PopupWindow({ open, setOpen }) {
     setTask({ ...task, [name]: value });
   };
 
+  const handleClose = () => {
+    setOpen(false);
+    setError({ ...error, isError: false });
+  };
+
   const handleSubmit = () => {
     if (task.name && task.day) {
       setTaskListInit([...taskListInit, task]);
       setTask(initialTask);
+      setError({ ...error, isError: false });
       setOpen(false);
     } else {
       setError({ ...error, isError: true });
@@ -55,74 +61,68 @@ function PopupWindow({ open, setOpen }) {
   if (!open) return null;
 
   return ReactDOM.createPortal(
-      <div className="popup">
-        <div className="popup-container">
-          <header>
-            <h2>New Task</h2>
-          </header>
-          <div className="close-popup-container" onClick={() => setOpen(false)}>
-            <AiOutlineClose />
-          </div>
-          <div className="container task-name">
-            <input
-              type="text"
-              placeholder="Enter new task..."
-              name="name"
-              onChange={handleChange}
+    <div className="popup">
+      <div className="popup-container">
+        <header>
+          <h2>New Task</h2>
+        </header>
+        <div className="close-popup-container" onClick={() => handleClose()}>
+          <AiOutlineClose />
+        </div>
+        <div className="container task-name">
+          <input
+            type="text"
+            placeholder="Enter new task..."
+            name="name"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="container task-description">
+          <textarea
+            placeholder="Enter notes..."
+            name="notes"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className="container day-list">
+          <div className="button-container day">
+            <button className="select-btn" onClick={() => setDayTrigger(true)}>
+              Select day
+            </button>
+            {task.day ? <p>Day selected: {task.day}</p> : ""}
+            <DaysPopupWindow
+              trigger={dayTrigger}
+              setTrigger={setDayTrigger}
+              task={task}
+              setTask={setTask}
             />
           </div>
-          <div className="container task-description">
-            <textarea
-              placeholder="Enter notes..."
-              name="notes"
-              onChange={handleChange}
-            ></textarea>
-          </div>
-          <div className="container day-list">
-            <div className="button-container day">
-              <button
-                className="select-btn"
-                onClick={() => setDayTrigger(true)}
-              >
-                Select day
-              </button>
-              {task.day ? <p>Day selected: {task.day}</p> : ""}
-              <DaysPopupWindow
-                trigger={dayTrigger}
-                setTrigger={setDayTrigger}
-                task={task}
-                setTask={setTask}
-              />
-            </div>
-            <div className="button-container day">
-              <button
-                className="select-btn"
-                onClick={() => setListTrigger(true)}
-              >
-                Select list
-              </button>
-              {task.list ? <p>List selected: {task.list}</p> : ""}
-              <ListPopupWindow
-                trigger={listTrigger}
-                setTrigger={setListTrigger}
-                task={task}
-                setTask={setTask}
-              />
-            </div>
-          </div>
-
-          <div className="container new-task">
-            <button
-              type="submit"
-              className={task.name && task.day ? "create-btn" : "no-task-btn"}
-              onClick={handleSubmit}
-            >
-              Create task
+          <div className="button-container day">
+            <button className="select-btn" onClick={() => setListTrigger(true)}>
+              Select list
             </button>
-            {error.isError ? <p>{error.message}</p> : ""}
+            {task.list ? <p>List selected: {task.list}</p> : ""}
+            <ListPopupWindow
+              trigger={listTrigger}
+              setTrigger={setListTrigger}
+              task={task}
+              setTask={setTask}
+            />
           </div>
         </div>
-      </div>,
+
+        <div className="container new-task">
+          <button
+            type="submit"
+            className={task.name && task.day ? "create-btn" : "no-task-btn"}
+            onClick={handleSubmit}
+          >
+            Create task
+          </button>
+          {error.isError ? <p>{error.message}</p> : ""}
+        </div>
+      </div>
+    </div>,
     document.getElementById("portal-root")
   );
 }
